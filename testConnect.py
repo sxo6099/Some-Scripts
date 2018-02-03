31959e556e163d17c0eeddbe3ab4fd5be1405006
sxo6099 <shaneogdencny@gmail.com>
2018-02-02 19:06:48 -0500

    Add files via upload

--- testConnect.py
+++ testConnect.py
@@ -1,15 +1,31 @@
 import socket
+import sys
 
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+'''Simple utility used in various labs and projects to test connectivity of setup networks'''
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(1)
 
 while (1):
     testtype = ""
-    testtype = raw_input("Would you like to test (l)ocal connectivity, (r)emote connectivity, or (D)NS resolution?")
+    testtype = input("Would you like to test (l)ocal connectivity, (r)emote connectivity, or (D)NS resolution?")
     if (testtype == "l"):
-    	s.connect(("192.168.44.254", 80))
+        try:
+            s.connect(("192.168.44.254", 50000))
+            print("Local connection successful")
+        except:
+            print("Local connection unsuccessful")
     elif (testtype == "r"):
-    	s.connect(("8.8.8.8", 80))
+        try:
+            s.connect(("8.8.8.8", 50000))
+            print("Remote connection successful")
+        except:
+            print("Remote connection unsuccessful")
     elif (testtype == "D"):
-    	s.connect(("google.com", 80))
+        try:
+            s.connect(("google.com", 50000))
+            print("DNS resolution successful")
+        except:
+            print("DNS resolution unsuccessful")
     else:
     	print("given &test, need l, r, or D")
